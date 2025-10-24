401eaf2619903a3eb3f8f5521ba5db54ad0bf72d
Alfayed Dennita <alfayeddennita@gmail.com>
2025-10-24 11:01:40 +0700

    Configure initial configs.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,17 @@
 <script>
-	import favicon from '$lib/assets/favicon.svg';
-
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <meta
+    name="description"
+    content="Fayden World is Alfayed Dennita's main personal website that contains public personal information, such as personal description, software development portfolio, art gallery, and contact details."
+  />
+  <meta
+    name="keywords"
+    content="Alfayed Dennita, Muhammad Alfayed Dennita, Fayden, Fayden World, personal, portfolio, projects, contacts"
+  />
+  <title>Fayden World - Alfayed Dennita's Personal Website</title>
 </svelte:head>
 
 {@render children?.()}
