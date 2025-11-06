22a76578341623d41d33f8f9a58363ab7f7d4a71
Alfayed Dennita <alfayeddennita@gmail.com>
2025-11-06 14:36:40 +0700

    Refactor header

--- src/routes/components/layout/Header.svelte
+++ src/routes/components/layout/Header.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { page } from '$app/state';
   import Button from '$lib/components/ui/Button.svelte';
 
   let offsetHeight = $state();
@@ -10,26 +11,33 @@
 </script>
 
 <header class="header" bind:offsetHeight>
-  <a href="/" class="title" title="Fayden World">
-    <h1 class="title__text">
-      <span class="title__left">Fayden</span>
-      <span class="title__right">World</span>
-    </h1>
+  <a class="title" href="/" title="Fayden World">
+    <svelte:element
+      this={page.route.id === '/' ? 'h1' : 'h2'}
+      class="title__text"
+    >
+      <span class="title__text-left">Fayden</span>
+      <span class="title__text-right">World</span>
+    </svelte:element>
   </a>
 
-  <div class="bgm">
-    <audio src="/audios/hello-planet.mp3" bind:paused={isBGMPaused} loop
+  <div class="bgm header__bgm">
+    <audio
+      src="/audios/hello-planet.mp3"
+      volume={0.5}
+      bind:paused={isBGMPaused}
+      loop
     ></audio>
     <div class="bgm__info">
       <p class="bgm__info-title">
-        <i class="bgm__info-icon hn hn-music-solid"></i>
+        <i class="hn hn-music-solid bgm__info-title-icon"></i>
         BGM is {isBGMPaused ? 'Paused' : 'Playing'}
       </p>
       <p class="bgm__info-song">
         <b>*Hello, Planet.</b> by
         <a
-          href="https://sasakure.uk.com/"
           class="bgm__info-artist"
+          href="https://sasakure.uk.com/"
           target="_blank"
           rel="external"
           title="sasakure.UK"
@@ -40,8 +48,8 @@
     </div>
     <Button
       theme="secondary"
-      title={isBGMPaused ? 'Play BGM' : 'Pause BGM'}
       square
+      title={isBGMPaused ? 'Play BGM' : 'Pause BGM'}
       onclick={() => (isBGMPaused = !isBGMPaused)}
     >
       {#if isBGMPaused}
@@ -55,19 +63,25 @@
 
 <style>
   .header {
-    z-index: 9999;
+    z-index: var(--z-index-topmost);
     position: absolute;
     top: 0;
     inset-inline: 0;
     width: 100%;
     height: 96px;
-    max-width: 1400px;
+    max-width: var(--breakpoint-xxl);
+    margin: 0 auto;
     display: flex;
     justify-content: space-between;
     align-items: center;
     gap: 16px;
-    margin: 0 auto;
     padding-inline: 16px;
+  }
+
+  @media (max-width: 256px) {
+    .header {
+      justify-content: center;
+    }
   }
 
   @media (min-width: 768px) {
@@ -94,15 +108,15 @@
     text-decoration: none;
     transition:
       gap 0.25s,
-      padding 0.25s,
-      background 0.25s,
-      outline 0.25s;
+      padding-left 0.25s,
+      background-color 0.25s,
+      outline-color 0.25s;
   }
 
   .title:is(:hover, :focus-visible) {
     gap: 12px;
     padding-left: 12px;
-    background-color: rgba(var(--color-secondary-main-rgb), 0.15);
+    background-color: rgba(var(--color-secondary-main-rgb), 0.1);
   }
 
   .title:focus-visible {
@@ -122,12 +136,12 @@
     }
   }
 
-  .title__left {
+  .title__text-left {
     display: flex;
     align-items: center;
   }
 
-  .title__right {
+  .title__text-right {
     display: flex;
     align-items: center;
     padding-inline: 8px;
@@ -142,18 +156,26 @@
     gap: 16px;
   }
 
+  @media (max-width: 256px) {
+    .header__bgm {
+      display: none;
+    }
+  }
+
   .bgm__info {
     display: flex;
     flex-direction: column;
-    align-items: flex-end;
     line-height: 1.2;
     text-align: right;
   }
 
+  @media (max-width: 320px) {
+    .header__bgm .bgm__info {
+      display: none;
+    }
+  }
+
   .bgm__info-title {
-    display: flex;
-    align-items: center;
-    gap: 4px;
     font-family: var(--font-family-pixel);
     font-size: var(--font-size-semi-small);
     text-transform: uppercase;
@@ -165,13 +187,16 @@
     }
   }
 
-  .bgm__info-icon {
+  .bgm__info-title-icon {
     font-size: var(--font-size-very-small);
+    line-height: 1.8;
+    vertical-align: middle;
   }
 
   @media (min-width: 576px) {
-    .bgm__info-icon {
+    .bgm__info-title-icon {
       font-size: var(--font-size-small);
+      line-height: 1.7;
     }
   }
 
@@ -182,11 +207,12 @@
   .bgm__info-artist {
     border-radius: 2px;
     outline: 2px solid transparent;
-    color: var(--color-black-pure);
     text-decoration-color: transparent;
+    text-decoration-skip-ink: none;
+    color: var(--color-black-pure);
     transition:
-      outline 0.25s,
-      text-decoration 0.25s;
+      outline-color 0.25s,
+      text-decoration-color 0.25s;
   }
 
   .bgm__info-artist:is(:hover, :focus-visible) {
