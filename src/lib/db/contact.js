168991b3b8b05580237f91572c840f5d86cb4b7e
Alfayed Dennita <alfayeddennita@gmail.com>
2025-11-16 20:43:26 +0700

    Add content-type

--- src/lib/db/contact.js
+++ src/lib/db/contact.js
@@ -18,6 +18,9 @@ export async function sendContact({
   };
   const res = await fetch(`${PUBLIC_WEB3FORMS_BASE_URL}/submit`, {
     method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify(body),
   });
   const data = await res.json();
